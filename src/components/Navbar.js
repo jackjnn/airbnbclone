ac335d86bcc6ed661b2eeb6622fc0f1110ffd4aa
Jack Njoroge <jacknyange@fb.com>
2022-07-12 18:30:04 -0700

    made cards composable by adding props

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../style.css"
+
+export default function Navbar() {
+    return (
+        <nav >
+            <img src={require("../images/airbnb-logo.png")} className="nav--logo"/>
+        </nav>
+    )
+}
